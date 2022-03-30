f5b7e8a99f9aecfbebba87fccf34744170e2e892
daviddflix <david-972010@hotmail.com>
2022-03-30 10:21:33 -0300

    deleting browserouter from app.js and adding landing route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import Landing from './components/Landing/Landing.jsx'
+import Nav  from './components/Nav/Nav.jsx'
 import "./App.css";
-// import Landing from "./components/Landing/Landing.jsx";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home.jsx";
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
+    <div className="App">
+       <Switch>
           <Route exact path="/home" component={Home} />
         </Switch>
-      </div>
-    </BrowserRouter>
+        <Switch>
+          <Route exact path="/landing" component={Landing} />
+        </Switch>
+    </div>
   );
 }
 
