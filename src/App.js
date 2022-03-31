db397db66c8b74d7471e93554d0a19fda0f3c4e9
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-03-31 11:30:02 -0300

    commit actualizar
    Signed-off-by: MauricioRaulFerreyra <mauriciotauro82@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
-import './App.css';
+import './App.css'
 import Landing from './components/Landing/Landing.jsx'
-import {Router, Route} from 'react-router-dom'
-=======
-import "./App.css";
->>>>>>> ece08d7a882d4451c39e68cef8ac6f7f2b77bd7a
+import { Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-    
-  
-=======
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> ece08d7a882d4451c39e68cef8ac6f7f2b77bd7a
-    </div>
-  );
+function App () {
+  return <div className='App'></div>
 }
 
-export default App;
+export default App
