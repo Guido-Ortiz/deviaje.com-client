95f136bb0e8cefd1ea8b126a3679a9b0d829d6e6
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-05-16 17:47:57 -0300

    se cambio la ruta de backend

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:4001";   
+//axios.defaults.baseURL = "http://localhost:4001";
 //https://deviaje.herokuapp.com
-   
+axios.defaults.baseURL = "https://viajes.fly.dev";
+
 export const axiosWithOutToken = async (endpoint, data, method = "GET") => {
-   if (method === "GET") {
-     return await axios.get(endpoint);
-   } else {
-     return await axios.request(endpoint, {
-        method,
-        data
-     });
-   }
+  if (method === "GET") {
+    return await axios.get(endpoint);
+  } else {
+    return await axios.request(endpoint, {
+      method,
+      data,
+    });
+  }
 };
